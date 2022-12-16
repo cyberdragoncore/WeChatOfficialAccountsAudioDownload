7f5dc1d19f2281157d1ce72dac3b33709f9cb8eb
唐语杰 <onertype@outlook.com>
2022-12-16 09:38:55 +0800

    Update getWeChatAudio.zsh
    改进了代码。不用再保存临时文件也能对网页的内容进行查找。

--- getWeChatAudio.zsh
+++ getWeChatAudio.zsh
@@ -1,27 +1,29 @@
 #!/bin/zsh
-Time=$(date +%s)
-http --download $1 --output tmp${Time}.html
-#下载网页并保存，以供解析 ID
-# $1 是文章的网址
+
+function ScanWebPage {
+  WebPage=$(http $1 --body)
+  # 下载网页代码
+  VoiceID=$(echo ${WebPage} | rg voice_id)
+  VoiceName=$(echo ${WebPage} | rg msg_title)
+  # VoiceID=$(http $1 --body | rg voice_id)
+  # VoiceName=$(http $1 --body | rg msg_title)
+  VoiceID=${VoiceID%%\"\,\"sn\"*}
+  # 这里是从右向左过滤字符串
+  VoiceID=${VoiceID#*voice_id\"\:\"}
+  # 这里是从左向右过滤字符
+  VoiceName=${VoiceName%%\'.html*}
+  VoiceName=${VoiceName#*msg_title = \'}
+}
+
+echo "Please enter URL"
+read URL
 VoiceURL="http://res.wx.qq.com/voice/getvoice?mediaid="
-#微信的音频内容都有一段网址是相同的
-SavePath=~/Download/
-#保存的地址
-VoiceID=$(rg voice_id tmp${Time}.html)
-VoiceName=$(rg msg_title tmp${Time}.html)
-#用 rigrep 解析出 ID 和音频名称
-VoiceID=${VoiceID%%\"\,\"sn\"*}
-#这里是从右向左过滤字符串
-VoiceID=${VoiceID#*voice_id\"\:\"}
-#这里是从左向右过滤字符串
-VoiceName=${VoiceName%%\'.html*}
-VoiceName=${VoiceName#*msg_title = \'}
+# 微信的音频内容都有一段网址是相同的
+SavePath=~/storage/shared/Download/英语视频/
+# 保存文件的地址
+ScanWebPage ${URL}
 echo "VoiceID" ${VoiceID}
 echo "VoiceName" ${VoiceName}.mp3
-#输出音频的 ID 和名字
+# 输出音频的 ID 和名字
 http --download ${VoiceURL}${VoiceID} --output ${SavePath}${VoiceName}.mp3
-#下载音频
-#mv ${VoiceName}.mp3 ~/storage/shared/Download/英语视频/${VoiceName}.mp3
-#这样方便复制到音箱里面。
-rm tmp${Time}.html
-#删除保存的临时网页
+# 下载音频文件到指定目录
